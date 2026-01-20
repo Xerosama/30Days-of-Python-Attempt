a76e9cb11cd6d7780570be2c51cd0dc1f7566ed0
Xero <44435627+Xerosama@users.noreply.github.com>
2026-01-20 16:32:03 +0530

    Started 30 Days of Python Challenge
    This is day 1 commit

---
+++ Day1/helloworld.py
@@ -0,0 +1,37 @@
+#This is the start of my 30 days of python challenge.
+import math as m
+
+# Day 1 - 30DaysOfPython Challenge
+print(3 + 4)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(4 * 3)             # multiplication(*)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 % 4)             # modulus(%)
+print(3 // 4)            # Floor division operator(//)
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 4, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 4.7}))    # Set
+print(type((9.8, 3.14, 4.7)))    # Tuple
+
+print('Name')
+print('Family Name')
+print('Country')
+print('I am enjoying 30 days of python')
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('name'))
+print(type('family name'))
+print(type('Country'))
+
+dist = m.sqrt((10-2)**2 + (8-3)**2)
+print(dist)
+input()
