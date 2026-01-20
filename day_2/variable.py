8e08d0a2cd5e74b9e0f0f315451fd15cf64c42ec
Xero <44435627+Xerosama@users.noreply.github.com>
2026-01-20 18:54:57 +0530

    Completed Day 2 exercises

---
+++ day_2/variable.py
@@ -0,0 +1,53 @@
+#Day 2: 30 Days of python programming
+
+# Exercise Level 1print(type(first_name))
+first_name = 'Alex'
+last_name = 'Mercer'
+full_name ='Alex Mercer'
+country = 'Country'
+city = 'City'
+age = 25
+year = 2026
+is_married = True
+is_light_on = True
+planet, galaxy, universe= 'Earth', 'Milky way', 'Origin Universe'
+
+# Exercise Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_light_on))
+print(type(planet))
+print(type(galaxy))
+print(type(universe))
+
+print(len(first_name))
+print(len(last_name))
+
+
+num_one = 5
+num_two = 4
+total = num_one +num_two
+diff =num_one - num_two
+product =num_one*num_two
+division = num_one/num_two
+remainder = num_one%num_two
+exp = num_one**num_two
+floor_division = num_one//num_two
+radius = 30
+area_of_circle = 3.1416*(radius**2)
+circum_of_circle =2*3.1416*radius
+radius_input = int(input('Input the Radius of circle:\n'))25
+area_of_circle = 3.1416 *(radius_input**2)
+print('Area of circle is:\n',area_of_circle)
+
+first_name = input('First Name:')
+last_name = input('Last Name:')
+country = input('Country:')
+age = input('Age:')
+
