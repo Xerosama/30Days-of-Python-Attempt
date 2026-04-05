6a3e30fcc4588fb3236560fc9a1e5d7b64496e23
Xero <44435627+Xerosama@users.noreply.github.com>
2026-04-05 14:46:30 +0530

    Completed Day 17 Exercise
    It only had 1 question, I was expecting atleast 5-7 questions.

---
+++ day_17/exception_handle.py
@@ -0,0 +1,9 @@
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+
+*nordic_countries, es,ru = names
+print('Nordic:',nordic_countries,'\nEs:',es,'\nRU:',ru)
+
+#try: code block that runs normally
+#except: if exception occurs in try block, this part handles the exit
+#else: If try block runs without exception, then this code will run
+#finally: This code will always run
